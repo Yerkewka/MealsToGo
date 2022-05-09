496de864252d74c6e15cb955985d0632eee85714
Yerkebulan Serikbayev <y.serikbayev@ssa.group>
2022-05-09 12:16:22 +0600

    Infrastructure setup

---
+++ src/infrastructure/theme/index.ts
@@ -0,0 +1,20 @@
+import { colors } from "./colors";
+import { space, lineHeights } from "./spacing";
+import { sizes } from "./sizes";
+import { fonts, fontWeights, fontSizes } from "./fonts";
+
+export const theme = {
+  colors,
+  space,
+  lineHeights,
+  sizes,
+  fonts,
+  fontSizes,
+  fontWeights,
+};
+
+export type ThemeType = typeof theme;
+
+export interface CustomThemeProps {
+  theme: ThemeType;
+}
